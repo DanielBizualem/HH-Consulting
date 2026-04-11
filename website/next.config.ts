7878e9d84d2748b4eb3b1b80b92b3ea028ff2fb4
Daniel Bizualem <danishow375@gmail.com>
2026-04-11 15:27:38 +0300

    optimizing an image

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,   // ← This skips Sharp during build
+  },
 };
+
 
 export default nextConfig;
